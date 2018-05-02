8a65037a201a959771f9578c4a5cf06947730072
Jon Schulman <schulman99@gmail.com>
2018-05-02 01:15:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ function logShout(string){
   console.log (`${string}`.toUpperCase())
 }
 
-function logShout(string){
-  console.log (`${string}`.toUpperCase())
+function logWhispet(string){
+  console.log (`${string}`.toLowerCase())
 }
 
+/*
+describe('logWhisper(string)', function() {
+  it('calls console.log() its one argument in all lowercase', function() {
+    const spy = expect.spyOn(console, 'log').andCallThrough()
+
+    logWhisper('HELLO')
+
+    expect(spy).toHaveBeenCalledWith('hello')
+
+    console.log.restore()
+  })
+})
