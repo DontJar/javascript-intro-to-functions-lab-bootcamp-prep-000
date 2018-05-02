97c42a5ea76b420f43ca18977999c4381ed86694
Jon Schulman <schulman99@gmail.com>
2018-05-02 01:15:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,16 +13,3 @@ function logShout(string){
 function logWhispet(string){
   console.log (`${string}`.toLowerCase())
 }
-
-/*
-describe('logWhisper(string)', function() {
-  it('calls console.log() its one argument in all lowercase', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logWhisper('HELLO')
-
-    expect(spy).toHaveBeenCalledWith('hello')
-
-    console.log.restore()
-  })
-})
