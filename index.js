26adb4cf85d73c58e6e437c3281d7ac6d7282b91
Jon Schulman <schulman99@gmail.com>
2018-05-02 01:14:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,17 +10,7 @@ function logShout(string){
   console.log (`${string}`.toUpperCase())
 }
 
+function logShout(string){
+  console.log (`${string}`.toUpperCase())
+}
 
-/*
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
-*/
